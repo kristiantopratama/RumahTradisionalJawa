e802e634cf1f028cc2fef77715cb36bbe95e32ce
Kristianto <kristianto.pratama@ti.ukdw.ac.id>
2022-09-16 00:41:55 +0700

    SemanticWeb

--- src/views/Visualisasi.vue
+++ src/views/Visualisasi.vue
@@ -10,38 +10,47 @@
                         Home
                     </router-link>
 
-                    <router-link :to="{name: 'provinsi' }" class="provinsi">
-                        Provinsi
+                    <router-link :to="{name: 'upacaraadat' }" class="provinsi">
+                        Upacara
                     </router-link>
 
-                    <router-link :to="{name: 'ornamen' }" class="ornamen">
-                        Ornamen
+                    <router-link :to="{name: 'ragamhias' }" class="ornamen">
+                        Ragam&nbsp;Hias
                     </router-link>
 
                     <router-link :to="{name: 'visualisasi' }" class="visualisasi">
                         <b>Visualisasi</b>
                     </router-link>
 
-                    <a href="#" class="search">SEARCH</a>
+				    <a href="#" class="search" style="margin-right:2%;">SEARCH</a>
+
+                    <div class="search" style="top:2.5px; margin:0; line-height:0.5;">
+                        <input v-model="message" type="text" placeholder="Search" size="2" style="height:40%; width:110%;">
+                        <span @click="reloadPage" type="submit" style="font-size: 12px; height:40%; border-radius:0; border:2px solid; border-color:black; padding-bottom:4px;">
+                            <router-link :to="{ name: 'search', params: {id: message}}" style="text-align:center;">
+                                <b>&nbsp;Search&nbsp;Rumah&nbsp;</b>
+                            </router-link>
+                        </span>
+                    </div>
                 </div>
                 
             </div>
 		
 	    </nav>
 
-        <section>
+        <section style="height:120vh;">
 
             <div class="content1">
                 <span class="judul_visualisasi">
-                    <b>Visualiasi Ontologi Rumah Tradisional Indonesia</b>	
+                    <b>Visualiasi Ontologi Rumah Tradisional Jawa</b>	
                 </span>
 
                 <div>
-                    <img class="center" src="../assets/RumahTradisional.png" alt="">
+                    <img class="center_visualisasi" src="../assets/T-Box.jpg" alt="">
                 </div>
                 
                 <div class="deskripsi">
-                
+                    <br><br>
                 </div>	
             </div>
 
@@ -56,3 +65,40 @@
 
     </body>
 </template>
+<script>
+    import {mapState} from 'vuex'
+    import {mapActions} from 'vuex'
+    
+    
+    export default {
+    
+      mounted(){
+        this.$store.dispatch('searchRumah', this.$route.params.id),
+        this.$store.dispatch('getLabelClassRumah')
+         
+      },
+      computed:{
+          ...mapState([
+            'searchRumah',
+            'getLabelClassRumah'
+          ])
+      },
+      methods:{
+          ...mapActions([
+            'searchRumah',
+            'getLabelClassRumah'       
+          ]),
+    
+          reloadPage() {
+          window.location.reload();
+        }
+    
+      },
+      data(){
+        return{
+          message: '0'
+        }
+      },
+    
+    }
+    </script>
